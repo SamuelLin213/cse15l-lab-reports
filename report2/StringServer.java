bbfbc28942be89b01356954cccad741cf98bcb8c
samuelddlin@gmail.com <samuelddlin@gmail.com>
2023-10-18 17:28:23 -0700

    add function to add string

--- report2/StringServer.java
+++ report2/StringServer.java
@@ -1,31 +1,39 @@
 import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-    int num = 0;
+    ArrayList<String> strings = new ArrayList<String>(); // create new list for strings
+
+    public String getList()
+    {
+      String res = "";
+      int cnt = 1;
+
+      for(String s: strings):
+      {
+        res += Integer.toString(cnt++) + ". " + s + "\n";
+      }
+      return String.format(res);
+    }
 
     public String handleRequest(URI url) {
-        if (url.getPath().equals("/")) {
-            return String.format("Number: %d", num);
-        } else if (url.getPath().equals("/increment")) {
-            num += 1;
-            return String.format("Number incremented!");
+        if (url.getPath().equals("/add-message")) {
+          String[] parameters = url.getQuery().split("=");
+          if (parameters[0].equals("s")) {
+              String temp = parameters[1];
+              strings.add(temp); // add string to list
+              return getList();
+          }
         } else {
-            if (url.getPath().contains("/add")) {
-                String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
-                }
-            }
             return "404 Not Found!";
         }
     }
 }
 
-class NumberServer {
+class StringServer {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
